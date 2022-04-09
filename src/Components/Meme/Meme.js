5b651e1601f5c54cb66d2e79d453f939de9d0643
Henri Bresteau <henri.bresteau@gamil.com>
2022-04-09 10:49:50 +0200

    meme img state in object

--- src/Components/Meme/Meme.js
+++ src/Components/Meme/Meme.js
@@ -1,25 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 import memeData from "../../data/meme.js";
 
 export default function Meme() {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemeImages, setAllMemeImages] = useState(memeData);
+
+  function getMemeImage() {
+    const memesArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({ ...prevMeme, randomImage: url }));
+    console.log(url);
+  }
+
   return (
-    <form>
-      <div className="form-grid">
-        <div className="form-input-top">
-          <input type="text" />
+    <section>
+      <form>
+        <div className="form-grid">
+          <div className="form-input-top">
+            <input type="text" />
+          </div>
+          <div className="form-input-bot">
+            <input type="text" />
+          </div>
+          <div className="form-submit">
+            <input
+              type="button"
+              value="Get a new meme Image 🖼️"
+              className="btn"
+              onClick={getMemeImage}
+            />
+          </div>
         </div>
-        <div className="form-input-bot">
-          <input type="text" />
-        </div>
-        <div className="form-submit">
-          <input
-            type="button"
-            value="Get a new meme Image 🖼️"
-            className="btn"
-          />
-        </div>
-      </div>
-    </form>
+      </form>
+      <img src={meme.randomImage} alt="meme" className="meme-img" />
+    </section>
   );
 }
